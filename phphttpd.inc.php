d216088e373f9f449b31919b602a9f5aec58293d
Harald Lapp <harald.lapp@gmail.com>
2012-09-05 23:29:56 +0700

    line endings

--- phphttpd.inc.php
+++ phphttpd.inc.php
@@ -7,7 +7,7 @@ phphttpd
 Copyright (c) 2012, Harald Lapp <harald.lapp@gmail.com>.
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without 
+Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
 are met:
 
@@ -151,7 +151,7 @@ if (php_sapi_name() == 'cli') {
     exit(0);
 } elseif (php_sapi_name() != 'cli-server') {
     printf(
-        "unable to execute '%s' in environment '%s'\n", 
+        "unable to execute '%s' in environment '%s'\n",
         basename($argv[0]),
         php_sapi_name()
     );
